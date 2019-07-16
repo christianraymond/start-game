55a1ed327d8bb2c97eba7717b9cb2a9d6a57d66f
christianraymond <ombeninsnraymond@gmail.com>
2019-07-16 19:28:45 +0200

    fixed mixed up or default import error

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
+import Game from './Game'
 
-export function App({ initialData }) {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <h1>{initialData.appName}</h1>
-      This is a sample stateful and server-side rendered React application.
-      <br />
-      <br />
-      Here is a button that will track how many times you click it:
-      <br />
-      <br />
-      <button onClick={() => setCount(count + 1)}>{count}</button>
-    </div>
-  );
+const StarMatch = () => {
+	const [gameId, setGameId] = useState(1);
+	return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)}/>;
 }
+
+export function App({}) {
+  return (
+    <StarMatch/>
+  );
+
+}
+
+export default StarMatch;
+
